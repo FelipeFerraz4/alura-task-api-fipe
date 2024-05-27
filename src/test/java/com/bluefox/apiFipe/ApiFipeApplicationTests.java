de42f4c5bf674b802cea457b1495b4015d5ec10f
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-27 08:58:07 -0300

    init project

---
+++ src/test/java/com/bluefox/apiFipe/ApiFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluefox.apiFipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
